d518ee21be7385c92228ad0d7466c037d67316e8
Mohammad Uzair <muzair.bscs15seecs@seecs.edu.pk>
2018-02-16 22:48:49 +0500

    RMS Algorithm Added

--- schedule.cpp
+++ schedule.cpp
@@ -38,12 +38,30 @@ void get_process_info(int selected_algo){
   			cin >> burst_time[i];
   			remain_time[i] = burst_time[i];
 		}
+		else if (selected_algo > 2){
+			cout << "==> Execution time: ";
+			cin >> execution_time[i];
+			remain_time[i] = execution_time[i];
+				cout << "==> Period: ";
+				cin >> period[i];
+		}
 	}
+}
+
+//get maximum of three numbers
+int max(int a,int b, int c){
+        long max,lcom, count, flag=0;
+        if( a>=b && a>=c)
+                return max=a;
+        else if(b >=a && b>=c)
+                return max=b;
+        else if(c>=a && c>=b)
+                return max=c;
 }
 
 //calculating the observation time for scheduling timeline
 int get_observation_time(int selected_algo){
-	if (selected_algo <= 2){
+	if (selected_algo < 3){
 		int sum=0;
 		for(int i=0; i<num_of_process; i++){
 			sum += burst_time[i];
@@ -51,6 +69,9 @@ int get_observation_time(int selected_algo){
 		return sum;
 	}
 
+	else if(selected_algo == 3){
+		return max(period[0],period[1],period[2]);
+	}
 
 }
 
@@ -203,6 +224,43 @@ void round_robin(int time){
 	print_schedule(process_list,time);
 }
 
+void rate_monotonic(int time){
+	float utilization = 0;
+	for (int i = 0; i < num_of_process; i++){
+		utilization += (1.0*execution_time[i])/period[i];
+	}
+	int n = num_of_process;
+	if (utilization > n*(pow(2,1.0/n)-1)){
+		cout << endl << "Given problem is not schedulable under said scheduling algorithm." << endl;
+		exit(0);
+	}
+
+	int process_list[time] = {0}, min = 999, next_process = 0;
+	for (int i = 0; i < time; i++){
+		min = 1000;
+		for (int j = 0; j < num_of_process; j++){
+			if (remain_time[j] > 0){
+				if (min > period[j]){
+					min = period[j];
+					next_process = j;
+				}
+			}
+		}
+
+		if (remain_time[next_process] > 0){
+			process_list[i] = next_process+1; 	// +1 for catering 0 array index.
+			remain_time[next_process] -= 1;
+		}
+
+		for (int k = 0; k < num_of_process; k++){
+			if ((i+1) % period[k] == 0){
+				remain_time[k] = execution_time[k];
+				next_process = k;
+			}
+		}
+	}
+	print_schedule(process_list, time);
+}
 
 int main(int argc, char* argv[]) {
 	int option = 0;
@@ -211,6 +269,7 @@ int main(int argc, char* argv[]) {
 	cout << "-----------------------------" << endl;
 	cout << "1. First Come First Serve" << endl;
 	cout << "2. Round Robin" << endl;
+	cout << "3. Rate Monotonic Scheduling" << endl;
 	cout << "-----------------------------" << endl;
 	cout << "Select > "; cin >> option;
 	cout << "-----------------------------" << endl;
@@ -222,5 +281,7 @@ int main(int argc, char* argv[]) {
 	 	first_come_first_serve(observation_time);
 	else if (option == 2)
 		round_robin(observation_time);
+	else if (option == 3)
+		rate_monotonic(observation_time);
 	return 0;
 }
